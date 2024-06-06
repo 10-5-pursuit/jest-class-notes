ccceab84dd077fbfdc1bab2adabd4a75b0bf2124
Carlos Godoy <carlosgodoy@pursuit.org>
2024-06-06 15:49:52 -0400

    jest class notes

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -1,11 +1,21 @@
 const { sum, subtract } = require('../src/calculator')
 
+// When you run `npm test`, it will run ALL of the files inside of the __test__ directory. Even if the files are nested within subdirectories, Jest will run all of them.
+
 // Basic tests
 // test("adds two numbers together and returns the sum", () => {
 //     expect( sum(300, -150) ).toBe(150)
 // })
 
+// 'Describe' blocks take two arguments:
+// 1: a string detailing what file/function we are testing
+// 2: a callback function in which we write our tests ( it() blocks )
+// You can use as many 'describe' blocks as you'd like in a single file
 describe("sum()", () => {
+    // "it" blocks are for individual tests. They also take two arguments:
+    // 1: a string detailing what we are testing for
+    // 2: a callback where we write the test logic
+    // You can write as many tests inside a 'describe' block as you'd like
     it("should add two numbers together and return the sum", () => {
         const actual = sum(5, 5)
         const expected = 10
@@ -21,6 +31,7 @@ describe("sum()", () => {
     })
 })
 
+// Second describe block
 describe("subtract()", () => {
     it("should subtract one number from another number", () => {
         expect(subtract(10, 5)).toBe(5)
@@ -28,6 +39,7 @@ describe("subtract()", () => {
 })
 
 
+// Example of a better text where we break up the block `expected(getFullName(object)).toBe(string)` to be more readable
 // describe("getFullName()", () => {
 //     it("should return a string with the full name of the contact", () => {
 //       const input = { names: { first: "Jude", surname: "Pearson" } };
